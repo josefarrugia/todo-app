d566f957b14d84614b0ae48e8624a53f9d19c8b7
Jose Farrugia <jose@josefarrugia.com>
2024-07-14 15:39:51 +0100

    feat: add toast notification component

---
+++ src/components/Toast/ToastNotification.vue
@@ -0,0 +1,9 @@
+<template>
+  <aside class="fixed w-full h-full top-0 flex items-center justify-center z-10">
+    <div
+      class="bg-blue-500 max-w-96 p-4 text-white font-bold rounded-2xl shadow-2xl shadow-blue-400"
+    >
+      <slot />
+    </div>
+  </aside>
+</template>
