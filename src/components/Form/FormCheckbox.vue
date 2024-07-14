4a6e03ff5f0e7ae5445b61a21c640d61fbd89b95
Jose Farrugia <jose@josefarrugia.com>
2024-07-14 15:35:40 +0100

    feat: add form components

---
+++ src/components/Form/FormCheckbox.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+import type { FormCheckbox } from '@/types/form';
+
+const props = defineProps<FormCheckbox>();
+
+const inputCheckbox = defineModel('inputCheckbox');
+</script>
+
+<template>
+  <label class="flex items-center" :for="props.id">
+    <input
+      type="checkbox"
+      :name="props.name"
+      :id="props.id"
+      class="w-5 h-5"
+      v-model="inputCheckbox"
+    />
+    <span class="sr-only">{{ props.label }}</span>
+  </label>
+</template>
---
+++ src/components/Form/FormInputText.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+import type { FormInputText } from '@/types/form';
+
+const props = defineProps<FormInputText>();
+
+const inputValue = defineModel<string>('inputValue');
+</script>
+
+<template>
+  <label :for="props.id">
+    <input
+      class="appearance-none bg-blue-50 w-full hover:bg-blue-100 px-4 outline-none rounded-lg"
+      :class="{ 'line-through': props.hasStrikeThrough }"
+      type="text"
+      :name="props.name"
+      :id="props.id"
+      v-model="inputValue"
+      autocomplete="off"
+    />
+    <span class="sr-only">{{ props.label }}</span>
+  </label>
+</template>
