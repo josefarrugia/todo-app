10937dd863ebe464881f52c0f724e77b9fd9eff9
Jose Farrugia <jose@josefarrugia.com>
2024-07-15 10:32:37 +0100

    feat: add TodoAdd component

---
+++ src/components/Todo/TodoAdd.vue
@@ -0,0 +1,31 @@
+<script lang="ts" setup>
+import { ref } from 'vue';
+import { useTaskStore } from '@/stores/tasks';
+import ButtonAdd from '@/components/Button/ButtonAdd.vue';
+import FormInputText from '@/components/Form/FormInputText.vue';
+
+const storeTask = useTaskStore();
+
+const todo = ref('');
+
+const handleAddTask = async () => {
+  await storeTask.addTask(todo.value);
+
+  todo.value = '';
+};
+</script>
+
+<template>
+  <h2 class="text-blue-500 text-lg font-bold">Add new task</h2>
+  <form class="flex py-2 gap-2" action="" @submit.prevent="handleAddTask">
+    <FormInputText
+      class="flex-1"
+      name="todo-add"
+      id="todo-add"
+      label="Add new task"
+      v-model:input-value="todo"
+      placeholder="Add new task here..."
+    />
+    <ButtonAdd type="submit" class="border-none" />
+  </form>
+</template>
